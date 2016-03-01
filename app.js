c5d4b2cdd407462f8bb850a987bbd7cbf778b42d
Jason Seminara <jseminara@gmail.com>
2016-02-29 21:08:19 -0500

    updated app with promises
    added gitignore and updated package
    
    updated app with promises
    
    fixed some spacing

--- app.js
+++ app.js
@@ -1,123 +1,85 @@
+'use strict'
 var twilio = require('twilio');
-var http = require('http');
+var request = require('request');
 var parseString = require('xml2js').parseString;
-var cronJob = require('cron').CronJob;
+var CronJob = require('cron').CronJob;
 var async = require('async');
 
-var client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTHTOKEN);
+var client = twilio( process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTHTOKEN);
 var openWeatherKey = process.env.OPENWEATHER_KEY;
+var desiredLine = process.env.WATCHED_TRAIN;
+var userFirstName = process.env.USER_NAME;
+var userZip = process.env.USER_ZIP;
 
-var textJob = new cronJob('0 18 * * *', function() {
-  http.get('http://web.mta.info/status/serviceStatus.txt', function(response) {
-    var completeResponse = '';
-    response.on('data', function(chunk) {
-      completeResponse += chunk;
-    });
-    response.on('end',function() {
-      parseString(completeResponse, function(err, result) {
-        if(err) {
-          console.log('error: ', err);
-        }
-
-        var myTrain = result.service.subway[0].line
-          .filter( (train) => train.name[0] === '456' )
-          .map( cleanData );
-
-        console.log(myTrain);
-
-        http.get('http://api.openweathermap.org/data/2.5/weather?zip=10009&units=imperial&appid='+openWeatherKey, function(response) {
-          var completeWeather = '';
-          response.on('data', function(WeatherChunk) {
-            completeWeather += WeatherChunk;
-          });
-          response.on('end', function() {
-
-            var myWeather = JSON.parse(completeWeather);
-            console.log(myWeather);
-            var myMessage = 'Good Morning Bobby,\n\nCurrent Temp: '+myWeather.main.temp+'°F\n\n'+myTrain[0].name+' Status:\n'+myTrain[0].status;
-            client.sendMessage({
-              to: process.env.PERSONAL_NUMBER,
-              from: process.env.TWILIO_NUMBER,
-              body: myMessage
-            }, function(err,data) {
-              console.log('error: %s\ndata: %s', err, data);
-            });
-
-          });
-        }.bind(myTrain));
-      });
-    });
-  }).on('error', function(err) {
-    console.log('problem with request: ' + err.message);
-  });
-}, null, true);
 
 
 function cleanData(obj) {
-  Object.keys(obj).forEach(function(key) {
-    obj[key] = obj[key][0];
-  });
+  Object.keys(obj).forEach( key => obj[key]=obj[key][0] );
   return obj;
 }
 
-// function TextMessage() {
-//   this.train = {
-//     name: null,
-//     status: null
-//   };
-//   this.weather = {
-//     current_temp: null,
-//     min_temp: null,
-//     max_temp: null
-//   };
-// }
 
-// TextMessage.prototype.getTrain = function() {
-//   // var myMessage = this;
-//   // console.log(myMessage);
-//   http.get('http://web.mta.info/status/serviceStatus.txt', function(response) {
-//     var completeResponse = '';
-//     response.on('data', function(chunk) {
-//       completeResponse += chunk;
-//     });
-//     response.on('end',function() {
-//       parseString(completeResponse, function(err, result) {
-//         if(err) {
-//           console.log('error: ', err);
-//         }
-//         var myTrain = result.service.subway[0].line.filter(function(train) {
-//           return train.name[0] === '456';
-//         });
-//         cleanData(myTrain[0]);
-//         this.train.name = myTrain[0].name;
-//         this.train.status = myTrain[0].status;
-//         // console.log(myMessage);
-//       });
-//     });
-//   }.bind(this));
-// };
+/*Create a new promise for the MTA Info*/
+var getMTAInfo = new Promise( function(resolve, reject) { 
+  var trainData="";
+  request.get( 'http://web.mta.info/status/serviceStatus.txt' )
+    .on('error',reject)
+    .on('data', data=>trainData+=data )
+    
+    .on('end', ()=>{
+      parseString( trainData , (err,result)=>{
+        
+        if(err) reject(err);
+        
+        var parsedTrains = result.service.subway[0].line
+          .filter( (train) => train.name[0] === '123' )
+          .map( cleanData )
+        
+        // finish the promise
+        resolve(parsedTrains)
+      })
+    })
+}); 
 
-// TextMessage.prototype.getWeather = function() {
-//   // var myMessage = this;
-//   // console.log(myMessage);
-//    http.get('http://api.openweathermap.org/data/2.5/weather?zip=10009&units=imperial&appid='+openWeatherKey, function(response) {
-//     var completeWeather = '';
-//     response.on('data', function(WeatherChunk) {
-//       completeWeather += WeatherChunk;
-//     });
-//     response.on('end', function() {
-//       var weatherJSON = JSON.parse(completeWeather);
-//       // console.log(myMessage);
-//       this.weather.current_temp = weatherJSON.main.temp;
-//       this.weather.min_temp = weatherJSON.main.temp.temp_min;
-//       this.weather.max_temp = weatherJSON.main.temp.temp_max;
-//       // var myMessage = 'Good Morning Bobby,\n\nCurrent Temp: '+myWeather.main.temp+'°F\n\n'+myTrain[0].name+' Status:\n'+myTrain[0].status;
-//     });
-//   }.bind(this));
-// };
+/*Create a new promise for the Weather Info*/
+var getWeather = new Promise( function(resolve,reject){
+  var completeWeather='';
 
-// var a = new TextMessage();
-// a.getTrain();
-// // a.getWeather();
-// // async.parallel( [ a.getTrain, a.getWeather ], function(err, results){ if(err){ console.log(err);} console.log(results);} );
-// // console.log(a);
+  var qs = {
+    zip:userZip,
+    units:'imperial',
+    appid:openWeatherKey
+  };
+
+  request.get({url:'http://api.openweathermap.org/data/2.5/weather',qs:qs})
+    .on('error',reject)  
+    .on('data', data=>completeWeather+=data )
+
+    /* finish the promise*/
+    .on('end', ()=> 
+      resolve( JSON.parse(completeWeather) )
+    )
+});
+
+
+var getAllData = function(){
+  /*Do the last step when they're both done*/
+  Promise.all( [getMTAInfo,getWeather] )
+    .then( data=>{
+        var myTrain = data[0];
+        var myWeather = data[1];
+        var myMessage = 'Good Morning '+userFirstName+',\n\nCurrent Temp: '+myWeather.main.temp+'°F\n\n'+myTrain[0].name+' Status:\n'+myTrain[0].status;
+        client.sendMessage({
+            to: process.env.PERSONAL_NUMBER,
+            from: process.env.TWILIO_NUMBER,
+            body: myMessage
+          }, 
+          (err,data)=>err ? console.log('error:', err) : console.log('data:', data);
+        )
+      }, 
+      err=>console.log('bad',err) 
+    )
+}
+
+var job = new CronJob('00 30 07 * * 1-5', getAllData)
+job.start();
